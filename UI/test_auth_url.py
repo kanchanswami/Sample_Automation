5e06ac17dc7969189d1567f10f7f6d339a524eae
kanchan <sonawane.kanchan@gmail.com>
2026-07-03 12:34:35 +1000

    modified final

--- UI/test_auth_url.py
+++
@@ -1,5 +0,0 @@
-from helper_to_check_auth_url import *
-
-#valid username and valid password
-def test_url_vusername_vpassword():
-    assert check_url(username='admin',password='admin'), "failed due to auth error"
