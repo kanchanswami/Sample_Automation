f82dfc0285780f511a5af6c8493cfeb6188ebb6d
kanchan <sonawane.kanchan@gmail.com>
2026-07-04 18:44:33 +1000

    modified conftest.py again

--- UI/my_pack/conftest.py
+++ UI/my_pack/conftest.py
@@ -1,29 +1,19 @@
 import pytest
-from playwright.sync_api import sync_playwright
 
 def pytest_addoption(parser):
     parser.addoption(
         "--browser",
         action="store",
         default="chromium",
-        help="Browser: chromium, firefox, or webkit"
+        help="Browser to run tests"
     )
 
 @pytest.fixture(scope="session")
 def browser_name(request):
     return request.config.getoption("--browser")
 
-@pytest.fixture(scope="session")
-def browser(browser_name):
-    with sync_playwright() as p:
-        if browser_name == "chromium":
-            browser = p.chromium.launch(headless=False)
-        elif browser_name == "firefox":
-            browser = p.firefox.launch(headless=False)
-        elif browser_name == "webkit":
-            browser = p.webkit.launch(headless=False)
-        else:
-            raise ValueError(f"Unsupported browser: {browser_name}")
-
-        yield browser
-        browser.close()
+@pytest.fixture()
+def setup():
+    print("Setup environment...")
+    yield
+    print("Tear Down..")
