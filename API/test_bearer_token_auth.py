1d895ba2de9754cd3085b25c2f00f22b8af92211
kanchan <sonawane.kanchan@gmail.com>
2026-07-20 18:04:00 +1000

    updated API requirements.txt

--- API/test_bearer_token_auth.py
+++ API/test_bearer_token_auth.py
@@ -1,7 +1,10 @@
 from playwright.sync_api import Playwright
+import os
 
 def test_bearer_token_auth(playwright:Playwright):
-    token ="xxxxxxx"
+    
+
+    token = os.getenv("GITHUB_TOKEN")
     request_context=playwright.request.new_context()
     response = request_context.get(url="https://api.github.com/user", headers={"Authorization": f"Bearer {token}"
                                     })
